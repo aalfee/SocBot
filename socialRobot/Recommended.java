7f7adecb55ac078a02c5041703fbfa877f132995
Alfiya Valitova <alfiyav6@gmail.com>
2025-01-12 09:58:40 -0500

    Jan twelve 25 changes with packages

--- socialRobot/Recommended.java
+++
@@ -1,7 +0,0 @@
-public interface Recommended{
-    public static final int DEFAULT_COUNT = 1;                  \\tentative
-    public static final int DEFAULT_RECOMMENDED_NEED_VALUE = 2;
-    public int recommendedNeedValue ();
-    //public int date();
-    public boolean does_expire(); 
-}
